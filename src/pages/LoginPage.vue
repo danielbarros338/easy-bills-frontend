07cb8397a4f81f901dfd94a65fc9a03bc2e09664
Daniel <usrrufus12@omint.com.br>
2024-02-29 17:34:18 -0300

    Created page component and route

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>login</div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LoginComponent",
+  setup() {},
+});
+</script>
